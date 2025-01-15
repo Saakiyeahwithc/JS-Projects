5d8da16539cd2ca97147af4c52ea4270fddc06c0
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2025-01-15 22:53:24 +0545

    updatedUIandhandlingfunctions

--- QuizApp/javascript/script.js
+++ QuizApp/javascript/script.js
@@ -1,22 +1,49 @@
 const answerOptions = document.querySelector(".answer-options");
 const nextQuestionBtn = document.querySelector(".next-question-btn");
+const questionStatus = document.querySelector(".question-status");
 
 let quizCategory = "entertainment";
 let currentQuestion = null;
+let numberOfQuestions = 10;
+const questionIndexHistory = [];
 
 //Fetch a random question from based on the selected category
 const getRandomQuestion = () => {
     const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || [];
-    const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)]
+
+    //Show the result if all the questions have been completed.
+    if (questionIndexHistory.length >= Math.min(categoryQuestions.length, numberOfQuestions)) {
+        return console.log("Quiz Completed!");
+    }
+
+    //Filter out already asked questions and choose a random one
+    const availableQuestions = categoryQuestions.filter((_, index) => !questionIndexHistory.includes(index));
+    const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)];
+
+    questionIndexHistory.push(categoryQuestions.indexOf(randomQuestion));
     return randomQuestion;
 }
 
+//highlight correct answer
+const highlightCorrectAnswer = () => {
+    const correctOption = answerOptions.querySelectorAll(".answer-option")[currentQuestion.correctAnswer];
+    correctOption.classList.add("correct");
+    const iconHTML = `<span class="material-symbols-outlined">${isCorrect ? 'check_circle' : 'cancel'}</span>`;
+    option.innerAdjacentHTML("beforeend", iconHTML);
+}
+
+//handle user's answer selection
 const handleAnswer = (option, answerIndex) => {
     const isCorrect = currentQuestion.correctAnswer === answerIndex;
     option.classList.add(isCorrect ? 'correct' : 'incorrect');
+    !isCorrect ? highlightCorrectAnswer() : "";
+
+    //Insert icon based on correctness
+    const iconHTML = `<span class="material-symbols-outlined">${isCorrect ? 'check_circle' : 'cancel'}</span>`;
+    option.innerAdjacentHTML("beforeend", iconHTML);
 
     //Disable all other answer option when one option is selected
-    answerOptions.querySelectorAll(".answer-option").forEach(option=>option.computedStyleMap.pointerEvents = "none");
+    answerOptions.querySelectorAll(".answer-option").forEach(option => option.style.pointerEvents = "none");
 }
 
 //render the current question and its options in the quiz
@@ -29,6 +56,7 @@ const renderQuestion = () => {
     //Update the UI
     answerOptions.innerHTML = "";
     document.querySelector(".question-text").textContent = currentQuestion.question;
+    questionStatus.innerHTML = `<b>${questionIndexHistory.length}</b> of <b>${numberOfQuestions}</b> Questions`;
 
     //Create option <li> elements, append them, and add click event Listners
     currentQuestion.options.forEach((option, index) => {
